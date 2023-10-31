bb43868f3e870870fe9d6d12fb9961382d530265
sazarrama <srhadair@gmail.com>
2023-10-31 15:17:35 +0000

    footer script and html update

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 /* Loads footer from footer.html */
 
-document.addEventListener("DOMContentLoaded", function () {
-        fetch("footer.html") // Path to your footer file
-            .then(response => response.text())
-            .then(data => {
-                // Insert the footer HTML into the div with id "footer-container"
-                document.getElementById("footer-container").innerHTML = data;
-            })
-            .catch(error => console.error("Error fetching footer: " + error));
-});
+function loadFooter() {
+    fetch("footer.html") // Path to your footer file
+        .then(response => response.text())
+        .then(data => {
+            // Insert the footer HTML into the div with id "footer"
+            document.getElementById("footer").innerHTML = data;
+        })
+        .catch(error => console.error("Error fetching footer: " + error));
+}
+
 
