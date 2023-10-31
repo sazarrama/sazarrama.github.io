820ef74ca926929a9226422afe756e776fb18ef1
sazarrama <srhadair@gmail.com>
2023-10-31 16:05:04 +0000

    footer css

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 /* Loads footer from footer.html */
 
-function loadFooter() {
-    fetch("footer.html") // Path to your footer file
-        .then(response => response.text())
-        .then(data => {
-            // Insert the footer HTML into the div with id "footer"
-            document.getElementById("footer").innerHTML = data;
-        })
-        .catch(error => console.error("Error fetching footer: " + error));
-}
+fetch("https://sazarrama.github.io/footer.html") // Use your GitHub Pages URL
+    .then(response => response.text())
+    .then(data => {
+        // Insert the footer HTML into the div with id "footer"
+        document.getElementById("footer").innerHTML = data;
+    })
+    .catch(error => console.error("Error fetching footer: " + error)
+);
+
 
 
