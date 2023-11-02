7afec39e2e9fbc7af57079484c8e53e6d6746871
sazarrama <srhadair@gmail.com>
2023-11-02 16:30:55 +0000

    loading for all pages

--- app.js
+++ app.js
@@ -1,50 +1,41 @@
-// Load footer from footer.html and common content from common.html when the page loads
 window.onload = function () {
-    loadFooter();
-    loadCommon();
-
-    if (window.location.href.includes('portfolio.html')) {
-        loadFolio();
-    }
+    showLoading(); // Show loading page initially
+    loadFooterAndCommon();
 }
 
-function loadCommon() {
-    fetch("https://sazarrama.github.io/common.html")
-        .then(response => response.text())
-        .then(data => {
-            // Create a temporary container to hold the common HTML content
-            const commonContainer = document.createElement('div');
-            commonContainer.innerHTML = data;
+function loadFooterAndCommon() {
+    Promise.all([
+        fetch("https://sazarrama.github.io/common.html").then(response => response.text()),
+        fetch("https://sazarrama.github.io/footer.html").then(response => response.text())
+    ]).then(([commonData, footerData]) => {
+        // Create a temporary container to hold the common HTML content
+        const commonContainer = document.createElement('div');
+        commonContainer.innerHTML = commonData;
 
-            // Insert the common content at the beginning of the body
-            document.body.insertBefore(commonContainer, document.body.firstChild);
-        })
-        .catch(error => console.error(error));
+        // Insert the common content at the beginning of the body
+        document.body.insertBefore(commonContainer, document.body.firstChild);
+
+        // Insert the footer HTML into the div with id "footer"
+        const footerElement = document.getElementById("footer");
+        if (footerElement) {
+            footerElement.innerHTML = footerData;
+        } else {
+            console.warn("Footer element not found.");
+        }
+
+        hideLoading(); // Hide loading page once content is loaded
+        showContent(); // Show the main content
+    }).catch(error => console.error(error));
 }
 
-function loadFooter() {
-    fetch("https://sazarrama.github.io/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            // Insert the footer HTML into the div with id "footer"
-            const footerElement = document.getElementById("footer");
-            if (footerElement) {
-                footerElement.innerHTML = data;
-            } else {
-                console.warn("Footer element not found.");
-            }
-        })
-        .catch(error => {
-            console.error("Error fetching footer: " + error);
-        });
+function showLoading() {
+    document.getElementById('loading').style.display = 'block';
 }
 
-function loadFolio() {
-    // Your existing code for portfolio.html
-    $('.carousel-item img').on('load', function () {
-        $(this).css('display', 'block');
-    });
+function hideLoading() {
+    document.getElementById('loading').style.display = 'none';
+}
 
-    document.getElementById('loading-spinner').style.display = 'none';
+function showContent() {
     document.getElementById('content').style.display = 'block';
 }
