dae3d23eac7c8b0cc41f667ed18c9d52feb6a3c7
sazarrama <srhadair@gmail.com>
2023-11-13 16:29:38 +0000

    pushing app.js file update

--- app.js
+++ app.js
@@ -37,7 +37,39 @@ function loadFooterAndCommon() {
 
         hideLoading(); // Hide the loading page once content is loaded
         showContent(); // Show the main content
+        loadImages(); // Load images and thumbnails dynamically
     }).catch(error => console.error(error));
+}
+
+function loadImages() {
+    const imagePath = "https://api.github.com/repos/sazarrama/sazarrama.github.io/contents/portfolio";
+    const thumbnailsContainer = document.getElementById("thumbnailsContainer");
+
+    fetch(imagePath)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(file => {
+                if (file.type === "file" && file.name.endsWith(".jpg")) {
+                    const imageNumber = file.name.split(".")[0]; // Extracting the image number
+                    const thumbnailDiv = createThumbnailDiv(imageNumber);
+                    thumbnailsContainer.appendChild(thumbnailDiv);
+                }
+            });
+        })
+        .catch(error => console.error(error));
+}
+
+function createThumbnailDiv(imageNumber) {
+    const thumbnailDiv = document.createElement("div");
+    thumbnailDiv.classList.add("thumbnail");
+    thumbnailDiv.addEventListener("click", function () {
+        openCarousel(imageNumber);
+    });
+
+    const imageSrc = `https://raw.githubusercontent.com/sazarrama/sazarrama.github.io/main/portfolio/${imageNumber}.jpg`;
+    thumbnailDiv.innerHTML = `<img src="${imageSrc}" class="d-block w-100" alt="Image ${imageNumber}">`;
+
+    return thumbnailDiv;
 }
 
 window.addEventListener('load', function () {
@@ -56,15 +88,41 @@ function showContent() {
 function openCarousel(imageNumber) {
     const carouselContainer = document.getElementById('carouselContainer');
     const carouselImage = document.getElementById('carouselImage');
-    const imagePath = `image${imageNumber}.jpg`; // Replace with actual image paths
+    const imagePath = `https://raw.githubusercontent.com/sazarrama/sazarrama.github.io/main/portfolio/${imageNumber}.jpg`;
 
     carouselImage.src = imagePath;
     carouselContainer.style.display = 'flex';
 }
 
 function closeCarousel() {
-const carouselContainer = document.getElementById('carouselContainer');
-carouselContainer.style.display = 'none';
+    const carouselContainer = document.getElementById('carouselContainer');
+    carouselContainer.style.display = 'none';
 }
 
+// JavaScript to handle thumbnail click and update the carousel
+document.addEventListener('DOMContentLoaded', function() {
+    const thumbnails = document.querySelectorAll('.thumbnail');
+    const carouselInner = document.querySelector('.carousel-inner');
+    let counter = 1;
 
+    thumbnails.forEach(thumbnail => {
+          thumbnail.addEventListener('click', function() {
+            const imgSrc = this.dataset.imgSrc;
+            const carouselItem = document.createElement('div');
+            carouselItem.classList.add('carousel-item');
+            carouselItem.innerHTML = `<img src="${imgSrc}" class="d-block w-100" alt="Image ${counter}">`;
+  
+            // Remove any existing active item
+            const activeItem = carouselInner.querySelector('.carousel-item.active');
+            if (activeItem) {
+              activeItem.classList.remove('active');
+            }
+  
+            carouselInner.appendChild(carouselItem);
+            carouselItem.classList.add('active');
+            
+            // Increment the counter for the next image
+            counter++;
+        });
+    });
+});
