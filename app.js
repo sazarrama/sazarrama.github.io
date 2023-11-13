48c1a52d5bf6220d5901db0266bece8614989a28
sazarrama <srhadair@gmail.com>
2023-11-13 16:03:35 +0000

    reverting carousel

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-const imageUrls = [
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/MNV001.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/EB001.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/EB002.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/EB003.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/LW001.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/LW002.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/LOG001.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/MG001.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/MG002.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/MG003.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/MFA001.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/TWF001.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/TWF002.jpg?raw=true",
-    "https://github.com/sazarrama/sazarrama.github.io/blob/main/portfolio/TWF003.jpg?raw=true",
-];
-
 window.onload = function () {
     loadLoading(); // Load and show the loading page initially
     loadFooterAndCommon();
@@ -35,15 +18,16 @@ function loadLoading() {
 function loadFooterAndCommon() {
     Promise.all([
         fetch("https://sazarrama.github.io/common.html").then(response => response.text()),
-        fetch("https://sazarrama.github.io/footer.html").then(response => response.text()),
-        fetch("https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js").then(response => response.text()), // Add this line to fetch Popper.js
-        fetch("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js").then(response => response.text()) // Add this line to fetch Bootstrap JavaScript
-    ]).then(([commonData, footerData, popperData, bootstrapData]) => {
+        fetch("https://sazarrama.github.io/footer.html").then(response => response.text())
+    ]).then(([commonData, footerData]) => {
+        // Create a temporary container to hold the common HTML content
         const commonContainer = document.createElement('div');
         commonContainer.innerHTML = commonData;
 
+        // Insert the common content at the beginning of the body
         document.body.insertBefore(commonContainer, document.body.firstChild);
 
+        // Insert the footer HTML into the div with id "footer"
         const footerElement = document.getElementById("footer");
         if (footerElement) {
             footerElement.innerHTML = footerData;
@@ -51,14 +35,14 @@ function loadFooterAndCommon() {
             console.warn("Footer element not found.");
         }
 
-        // Execute Popper.js and Bootstrap JavaScript code
-        eval(popperData);
-        eval(bootstrapData);
-
-        hideLoading();
-        showContent();
+        hideLoading(); // Hide the loading page once content is loaded
+        showContent(); // Show the main content
     }).catch(error => console.error(error));
 }
+
+window.addEventListener('load', function () {
+    hideLoading(); // Call a function to hide the loading container
+});
 
 function hideLoading() {
     document.getElementById('loading-container').style.display = 'none';
@@ -68,88 +52,19 @@ function showContent() {
     document.getElementById('content').style.display = 'block';
 }
 
-// Function to create a carousel item
-function createCarouselItem(index) {
-    const carouselInner = document.querySelector('.carousel-inner');
+// Carousel script
+function openCarousel(imageNumber) {
+    const carouselContainer = document.getElementById('carouselContainer');
+    const carouselImage = document.getElementById('carouselImage');
+    const imagePath = `image${imageNumber}.jpg`; // Replace with actual image paths
 
-    // Check if the carouselInner element exists
-    if (carouselInner) {
-        const item = document.createElement('div');
-        item.className = 'carousel-item';
-        const img = document.createElement('img');
-        img.src = imageUrls[index];
-        img.className = 'd-block w-100';
-        img.alt = `Image ${index + 1}`;
-        item.appendChild(img);
-        carouselInner.appendChild(item); // Append the item to the carousel inner container
-    } else {
-        console.error("Carousel inner container not found.");
-    }
+    carouselImage.src = imagePath;
+    carouselContainer.style.display = 'flex';
 }
 
-// Function to generate carousel items
-function generateCarouselItems() {
-    const carouselInner = document.querySelector('.carousel-inner');
-
-    // Check if the carouselInner element exists
-    if (carouselInner) {
-        for (let i = 0; i < imageUrls.length; i++) {
-            createCarouselItem(i);
-        }
-    } else {
-        console.error("Carousel inner container not found.");
-    }
+function closeCarousel() {
+const carouselContainer = document.getElementById('carouselContainer');
+carouselContainer.style.display = 'none';
 }
 
-// Call the function to generate carousel items
-generateCarouselItems();
 
-// Initialize Bootstrap Carousel
-document.addEventListener('DOMContentLoaded', function () {
-    const carousel = new bootstrap.Carousel(document.getElementById('carouselExample'), {
-        interval: false, // Disable automatic cycling
-    });
-
-    // Call the function to generate carousel items
-    generateCarouselItems();
-
-    // Carousel script
-    window.openCarousel = function (imageNumber) {
-        // Adjusted index to match array (1-indexed)
-        const currentIndex = imageNumber - 1;
-
-        // Activate the corresponding carousel item
-        carousel.to(currentIndex);
-
-        // Display the carousel
-        document.getElementById('carouselContainer').style.display = 'flex';
-    };
-
-    // Close the carousel
-    window.closeCarousel = function () {
-        document.getElementById('carouselContainer').style.display = 'none';
-    };
-});
-
-// Function to create a thumbnail element
-function createThumbnail(index) {
-    const thumbnailContainer = document.getElementById('gridContainer');
-    const thumbnail = document.createElement('div');
-    thumbnail.className = 'thumbnail';
-    thumbnail.onclick = function() {
-        openCarousel(index + 1);
-    };
-    const img = document.createElement('img');
-    img.src = imageUrls[index];
-    img.className = 'd-block w-100';
-    img.alt = `Image ${index + 1}`;
-    thumbnail.appendChild(img);
-    thumbnailContainer.appendChild(thumbnail);
-}
-
-// Function to generate thumbnails
-function generateThumbnails() {
-    for (let i = 0; i < imageUrls.length; i++) {
-        createThumbnail(i);
-    }
-}
