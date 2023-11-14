2a94e0b26515711153639f81b95e0d8828f7d638
sazarrama <srhadair@gmail.com>
2023-11-14 11:17:19 +0000

    debugging

--- app.js
+++ app.js
@@ -18,14 +18,15 @@ function loadJQuery(callback) {
     }
 }
 
+var imageIndex = $(this).data("image-index");
+
 // After jQuery is loaded, load other scripts
 loadJQuery(function () {
     // jQuery-dependent code inside $(document).ready()
     $(function () {
         // Event listener for thumbnail clicks using event delegation
         $(document).on("click", ".thumbnail", function () {
-            var imageIndex = $(this).data("image-index");
-
+            
             // Open the carousel with the selected image index
             openCarousel(imageIndex);
         });
@@ -89,8 +90,7 @@ function showContent() {
 const imagePath = Array.from({ length: 14 }, (_, i) => `https://raw.githubusercontent.com/sazarrama/sazarrama.github.io/main/portfolio/${i + 1}.jpg`);
 
 function loadImages() {
-    // Fetch all image URLs for the carousel
-
+    const imagePath = "https://api.github.com/repos/sazarrama/sazarrama.github.io/contents/portfolio";
     const thumbnailsContainer = document.getElementById("grid-container");
 
     if (!thumbnailsContainer) {
@@ -102,36 +102,47 @@ function loadImages() {
     thumbnailsContainer.innerHTML = "";
 
     fetch(imagePath)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
-            console.log("Data received from the API:", data);
+            console.log("Raw API response:", data); // Log the raw API response
 
             data.forEach(file => {
                 if (file.type === "file" && file.name.endsWith(".jpg")) {
                     const imageNumber = file.name.split(".")[0];
-                    const thumbnailDiv = createThumbnailDiv(imageNumber, imagePaths);
+                    const thumbnailDiv = createThumbnailDiv(imageNumber);
                     thumbnailsContainer.appendChild(thumbnailDiv);
                 }
             });
 
-            for (let i = 1; i <= 14; i++) {
-                const thumbnailDiv = createThumbnailDiv(i);
-                thumbnailsContainer.appendChild(thumbnailDiv);
-            }
-
             // Initialize the Bootstrap carousel
             $('#imageCarousel').carousel();
         })
-        .catch(error => console.error(error));
+        .catch(error => {
+            console.error("Error fetching images:", error);
+        });
 }
 
-function createThumbnailDiv(imageNumber, imagePaths) {
+function createThumbnailDiv(imageNumber) {
     const thumbnailDiv = document.createElement("div");
     thumbnailDiv.classList.add("thumbnail");
+
+    // Log the imagePaths array for debugging
+    console.log("imagePaths:", imagePaths);
+
     const imageSrc = imagePaths[imageNumber - 1]; // Adjust the index
-    thumbnailDiv.innerHTML = `<img src="${imageSrc}" class="d-block w-100" alt="Image ${imageNumber}" onclick="openCarousel(${imageNumber}, ${JSON.stringify(imagePaths)})">`;
+
+    // Log the imageSrc for debugging
+    console.log("imageSrc:", imageSrc);
+
+    thumbnailDiv.innerHTML = `<img src="${imageSrc}" class="d-block w-100" alt="Image ${imageNumber}" onclick="openCarousel(${imageNumber})">`;
     return thumbnailDiv;
 }
+
 
 window.addEventListener('load', function () {
     hideLoading();
